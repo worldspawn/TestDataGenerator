e7eaa9000eeff9505d4b3e4181fc162bb70ebb59
Sam Critchley <critchley.sj@gmail.com>
2012-07-09 14:55:11 +1000

    set version to alpha for nuget

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -2,5 +2,6 @@ using System.Reflection;
 
 [assembly: AssemblyDescription("Object graph config and generation library useful for unit testing.")]
 [assembly: AssemblyCompany("Sam Critchley")]
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyInformationalVersion("0.1.0.0-alpha")]
